fe516624c72c6cb9bc5dd0a0e4170ea8912c1be1
Alexey Peshekhonov <peshehonov.alexey@gmail.com>
2021-06-07 21:04:41 +0200

    Initial Commit

---
+++ Ride-Foodv02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ride-Foodv02
+//
+//  Created by Alexey Peshekhonov on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
