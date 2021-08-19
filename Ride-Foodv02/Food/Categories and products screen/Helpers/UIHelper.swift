af1f7f8c523a2b1ad9bb716919056c68e70ca107
VBel3235 <Vlad95.0753@gmail.com>
2021-08-19 11:32:44 +1000

    Further improved subcategories and products screen, screens are refreshing

--- Ride-Foodv02/Food/Categories and products screen/Helpers/UIHelper.swift
+++ Ride-Foodv02/Food/Categories and products screen/Helpers/UIHelper.swift
@@ -33,7 +33,7 @@ struct UIHelper {
         
         let flowLayout                  = UICollectionViewFlowLayout()
         flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 60)
         
         return flowLayout
     }
--- Ride-Foodv02/Helpers/Extensions/UIView+Extensions.swift
+++ Ride-Foodv02/Helpers/Extensions/UIView+Extensions.swift
@@ -13,6 +13,15 @@ extension UIView {
             return instanceFromNib()
         }
     
+
+           public func addShadowToView(shadow_color: UIColor,offset: CGSize,shadow_radius: CGFloat,shadow_opacity: Float,corner_radius: CGFloat) {
+               self.layer.shadowColor = shadow_color.cgColor
+               self.layer.shadowOpacity = shadow_opacity
+               self.layer.shadowOffset = offset
+               self.layer.shadowRadius = shadow_radius
+               self.layer.cornerRadius = corner_radius
+           }
+       
 }
 
 extension UINib {
